81a94b9378710fd75043f6427cdd2c519140836c
Sam Watts <samuel.watts@gmail.com>
2017-01-25 10:05:38 +1100

    add new key window when showing veneer

--- Veneer/Classes/UIViewController+Veneer.swift
+++ Veneer/Classes/UIViewController+Veneer.swift
@@ -11,6 +11,20 @@ import UIKit
 public extension UIViewController {
     
     public func showVeneer() {
+        guard self.veneerWindow == nil else {
+            print("Error: unable to show veneer when one has already been shown")
+            return
+        }
         
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window.windowLevel = UIWindowLevelStatusBar + 1
+        
+        window.rootViewController = VeneerRootViewController()
+        
+        window.makeKeyAndVisible()
+        
+        //store window to prevent it being removed on deinit
+        self.veneerWindow = window
     }
 }
