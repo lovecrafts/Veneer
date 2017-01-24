2bff8f49dda03c5f1e0ca90fd5b534e03d2295a3
Sam Watts <samuel.watts@gmail.com>
2017-01-24 17:53:15 +1100

    pod install and migrated to swift 3

---
+++ Example/Pods/Target Support Files/Pods-Veneer_Example/Pods-Veneer_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Veneer_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Veneer_Example
+@end
---
+++ Example/Pods/Target Support Files/Veneer/Veneer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Veneer : NSObject
+@end
+@implementation PodsDummy_Veneer
+@end
