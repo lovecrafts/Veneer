22aa40ce3757967f0dec8b289e636d03a4924518
Sam Watts <samuel.watts@gmail.com>
2017-01-25 09:59:48 +1100

    Added UIViewController extension as main point of entry

---
+++ Veneer/Classes/UIViewController+Veneer.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController+Veneer.swift
+//  Pods
+//
+//  Created by Sam Watts on 25/01/2017.
+//
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showVeneer() {
+        
+    }
+}
