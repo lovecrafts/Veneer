137954c0002d76811a2c775fd23f82dc0c77a41f
Sam Watts <samuel.watts@gmail.com>
2017-01-25 09:57:27 +1100

    added overlay root view controller

---
+++ Veneer/Classes/VeneerRootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VeneerRootViewController.swift
+//  Pods
+//
+//  Created by Sam Watts on 25/01/2017.
+//
+//
+
+import UIKit
+
+class VeneerRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+    }
+
+}
