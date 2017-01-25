bae5c024e6941aa729d5fffdbe1a37976ff5154f
Sam Watts <samuel.watts@gmail.com>
2017-01-25 14:35:36 +1100

    use more sensible defaults for colours / dash pattern etc

--- Veneer/Classes/HighlightView.swift
+++ Veneer/Classes/HighlightView.swift
@@ -16,7 +16,33 @@ class HighlightView: UIView {
     @available(*, unavailable, message: "init?(coder:) is unavailable, use init(highlight:) instead")
     required init?(coder aDecoder: NSCoder) { fatalError() }
     
+    dynamic var lineDashColor: UIColor = .black
+    dynamic var lineDashPattern: [Int] = [10, 10]
+    dynamic var lineDashWidth: CGFloat = 10
+    
+    lazy private(set) var borderLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.fillColor = UIColor.clear.cgColor
+        return layer
+    }()
+    
     init(highlight: Highlight) {
         super.init(frame: .zero)
+        
+        self.layer.addSublayer(borderLayer)
+        
+        borderLayer.strokeColor = lineDashColor.cgColor
+        borderLayer.lineDashPattern = lineDashPattern as [NSNumber]
+        borderLayer.lineWidth = lineDashWidth
+    }
+    
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        guard layer == self.layer else { return }
+        
+        borderLayer.frame = layer.bounds
+        
+        let path = UIBezierPath(rect: layer.bounds)
+        borderLayer.path = path.cgPath
     }
 }
