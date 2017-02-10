ac8761a400023a76d360589a95256eb453697cda
Sam Watts <samuel.watts@gmail.com>
2017-02-10 09:58:14 +0000

    updated path for outline for views types

--- Veneer/Classes/HighlightView.swift
+++ Veneer/Classes/HighlightView.swift
@@ -22,7 +22,11 @@ public class HighlightView: UIView {
         return layer
     }()
     
+    private let highlight: Highlight
+    
     init(highlight: Highlight) {
+        self.highlight = highlight
+        
         super.init(frame: .zero)
         
         self.layer.addSublayer(borderLayer)
@@ -43,7 +47,78 @@ public class HighlightView: UIView {
                 
         borderLayer.frame = lineBounds
         
-        let path = UIBezierPath(rect: lineBounds)
-        borderLayer.path = path.cgPath
+        switch highlight.viewType {
+        case .viewUnion(let views):
+            borderLayer.path = enclosingPathForViews(views: views).cgPath
+        default:
+            borderLayer.path = UIBezierPath(rect: lineBounds).cgPath
+        }
+    }
+    
+    func enclosingPathForViews(views: [UIView]) -> UIBezierPath {
+        
+        let frames = views.map { self.convert($0.frame, from: $0.superview) }
+        
+        let path = UIBezierPath()
+        
+        // top left and right corners of each view
+        // sorted from left to right, top to bottom
+        var topPoints:[CGPoint] = frames.reduce([]) { $0 + [CGPoint(x: $1.minX, y: $1.minY), CGPoint(x: $1.maxX, y: $1.minY)] }
+        topPoints = topPoints.sorted(by: { $0.x == $1.x ? $0.y < $1.y : $0.x < $1.x })
+        
+        // trace top line from left to right
+        // moving up or down when appropriate
+        guard var previousPoint = topPoints.first else { return path }
+        path.move(to: previousPoint)
+        for point in topPoints {
+            
+            guard point.y == previousPoint.y
+                || point.y < previousPoint.y
+                && frames.contains(where: {$0.minX == point.x && $0.minY < previousPoint.y })
+                || point.y > previousPoint.y
+                && !frames.contains(where: { $0.maxX > point.x && $0.minY < point.y })
+                else  { continue }
+            
+            if point.y < previousPoint.y {
+                path.addLine(to: CGPoint(x:point.x, y:previousPoint.y))
+            } else if point.y > previousPoint.y {
+                path.addLine(to: CGPoint(x:previousPoint.x, y:point.y))
+            }
+            
+            path.addLine(to: point)
+            previousPoint = point
+        }
+        
+        // botom left and right corners of each view
+        // sorted from right to left, bottom to top
+        var bottomPoints: [CGPoint] = frames.reduce([]) { $0 + [CGPoint(x: $1.minX, y: $1.maxY), CGPoint(x: $1.maxX, y: $1.maxY)] }
+        bottomPoints = bottomPoints.sorted(by: { $0.x == $1.x ? $0.y > $1.y : $0.x > $1.x })
+        
+        // trace bottom line from right to left
+        // starting where top line left off (rightmost top corner)
+        // moving up or down when appropriate
+        for point in bottomPoints {
+            
+            guard point.y == previousPoint.y
+                || point.y > previousPoint.y
+                && frames.contains(where: {$0.maxX == point.x && $0.maxY > previousPoint.y })
+                || point.y < previousPoint.y
+                && !frames.contains(where: { $0.minX < point.x && $0.maxY > point.y })
+                else  { continue }
+            
+            if point.y > previousPoint.y {
+                path.addLine(to: CGPoint(x:point.x, y:previousPoint.y))
+            } else if point.y < previousPoint.y {
+                path.addLine(to: CGPoint(x:previousPoint.x, y:point.y))
+            }
+            
+            path.addLine(to: point)
+            previousPoint = point
+        }
+        
+        // close back to leftmost point of top line
+        path.close()
+        
+        return path
     }
 }
