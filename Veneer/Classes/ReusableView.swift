4f4045698b9491de8a035dfb285073d161e031c7
Sam Watts <samuel.watts@gmail.com>
2017-03-13 12:31:19 +0000

    added tests for reusable view type

--- Veneer/Classes/ReusableView.swift
+++ Veneer/Classes/ReusableView.swift
@@ -10,7 +10,6 @@ import Foundation
 
 public protocol ReusableContainerView {
     
-    var containerView: UIView { get }  //return instance of backing view, e.g. collection or table view
     func reusableView(atIndexPath indexPath: IndexPath) -> UIView? //instance of cell to be tracked
 }
 
@@ -18,20 +17,12 @@ public protocol ReusableContainerView {
 
 extension UITableView: ReusableContainerView {
     
-    public var containerView: UIView {
-        return self
-    }
-    
     public func reusableView(atIndexPath indexPath: IndexPath) -> UIView? {
         return self.cellForRow(at: indexPath)
     }
 }
 
 extension UICollectionView: ReusableContainerView {
-    
-    public var containerView: UIView {
-        return self
-    }
     
     public func reusableView(atIndexPath indexPath: IndexPath) -> UIView? {
         return self.cellForItem(at: indexPath)
