24eaaa6589b405dcfc2c0f3e37fb1e683c19f949
Sam Watts <samuel.watts@gmail.com>
2017-03-13 09:08:13 +0000

    use reusable view type when highlighting collection view cell

--- Example/Veneer/ViewController.swift
+++ Example/Veneer/ViewController.swift
@@ -152,10 +152,10 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("showing overlay highlighting cell at index path: \(indexPath)")
+                
+        let viewType = Highlight.ViewType.reusableView(reusableView: collectionView, indexPath: indexPath)
         
-        guard let cell = collectionView.cellForItem(at: indexPath) else { return }
-        
-        let highlight = Highlight(viewType: .view(view: cell), cornerRadius: indexPath.item == 3 ? 6 : 0) { [weak self] dismissType in
+        let highlight = Highlight(viewType: viewType, cornerRadius: indexPath.item == 3 ? 6 : 0) { [weak self] dismissType in
             print("Dismissed with type: \(dismissType)")
             
             switch dismissType {
